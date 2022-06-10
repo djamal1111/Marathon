698f7c4bbbf6303d21df76763d748a5c22328d58
Джамал <97660596+djamal1111@users.noreply.github.com>
2022-06-10 11:58:08 +0300

    Add files via upload

---
+++ Marathon-main/Marathon/Marathon Skills 2022.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marathon_Selin
+{
+    public partial class Marathon_Skills_2022 : Form
+    {
+        public Marathon_Skills_2022()
+        {
+            InitializeComponent();
+        }
+    }
+}
